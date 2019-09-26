621713d2aa1a56343ce42179a3d7c5a966187fc9
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-26 23:28:02 +0200

    + go.bat

---
+++ csc.exe/-langversion/go.bat
@@ -0,0 +1,21 @@
+@rem
+@rem https://stackoverflow.com/a/58083788/180275
+@rem
+
+@csc -langversion:?
+@rem Supported language versions:
+@rem default
+@rem 1
+@rem 2
+@rem 3
+@rem 4
+@rem 5
+@rem 6
+@rem 7.0
+@rem 7.1
+@rem 7.2
+@rem 7.3
+@rem 8.0 (default)
+@rem latestmajor
+@rem preview
+@rem latest
