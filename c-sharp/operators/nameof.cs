8845b39b9a87b93705b297d87bd20a7b58fd56a1
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-05 14:24:32 +0200

    + nameof.cs

---
+++ c-sharp/operators/nameof.cs
@@ -0,0 +1,16 @@
+namespace TQ84 {
+
+   public class cls {
+
+      static void Main(string[] args) {
+
+      //
+      // nameof() is evaluated at compile time
+      //
+         System.Console.WriteLine("nameof(args) = " + nameof(args));
+
+      }
+
+   }
+
+}
