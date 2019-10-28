077436df20ab6c3637335e145e9edabe8bfb62ae
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-10-28 13:14:59 +0100

    n/a

---
+++ compilers/c-sharp/objects/fields-properties-etc/intro.cs
@@ -0,0 +1,27 @@
+public class C {
+
+   private string field;
+
+   public string prop {
+      get { return field;         }
+      set {        field = value; }
+   }
+
+// AutoProperty generates private field for us
+   public int auto_prop{get; set;}
+}
+
+public class M {
+
+   public static void Main() {
+
+      C c1 = new C();
+//    C c2 = new C();
+
+      c1.auto_prop = 42;
+      c1.prop      ="Hello World";
+
+      System.Console.WriteLine($"{c1.auto_prop} - {c1.prop}");
+
+   }
+}
