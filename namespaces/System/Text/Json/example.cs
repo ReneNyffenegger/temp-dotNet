61ad665f6803d98eec2ebdc6d5c3b38998363bc5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-06 20:57:58 +0100

    n/a

---
+++ namespaces/System/Text/Json/example.cs
@@ -0,0 +1,14 @@
+//
+//  https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+//
+
+using System.Text.Json;
+
+public class M {
+
+   public static void Main() {
+
+
+   }
+
+}
