ac4a7b89669bf95cc37e227cc4c7979e8f42b007
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-11 20:26:58 +0100

    + strings.cs

---
+++ compilers/c-sharp/strings.cs
@@ -0,0 +1,18 @@
+using System;
+
+class C {
+
+   static void Main() {
+
+      Console.WriteLine( " He said: \"Hello World\". ");      
+
+      Console.WriteLine(@" He said ""Hello World"". ");      
+
+      Console.WriteLine( " new\n line.");
+      Console.WriteLine(@" new
+ line.");
+//    Console.WriteLine(@" He said \"Hello World\". ");      
+
+   }
+
+}
