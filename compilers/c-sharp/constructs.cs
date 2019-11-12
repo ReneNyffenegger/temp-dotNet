b646c6499fa278bbfa7ed2a97e7c053af95b8e57
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-12 22:21:42 +0100

    Create constructs.cs

---
+++ compilers/c-sharp/constructs.cs
@@ -0,0 +1,28 @@
+internal static T TypeTableGetMemberDelegate<T>(PSObject msjObj, string name) where T : PSMemberInfo
+{
+    TypeTable table = msjObj.GetTypeTable();
+    return TypeTableGetMemberDelegate<T>(msjObj, table, name);
+}
+
+****
+
+
+private static T AdapterGetMemberDelegate<T>(PSObject msjObj, string name) where T : PSMemberInfo {
+ ...
+    T adaptedMember = msjObj.AdaptedMembers[name] as T;
+ ...
+}
+
+*****
+
+
+public PSObject()
+{
+    CommonInitialization(PSCustomObject.SelfInstance);
+}
+
+
+public PSObject(int instanceMemberCapacity) : this()
+{
+    _instanceMembers = new PSMemberInfoInternalCollection<PSMemberInfo>(instanceMemberCapacity);
+}
