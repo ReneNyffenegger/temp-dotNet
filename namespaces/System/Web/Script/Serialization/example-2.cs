a8890b4107f97a8f9c86ab6b29c7f5b7280469b2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-12 07:00:48 +0100

    n/a

--- namespaces/System/Web/Script/Serialization/example-2.cs
+++ namespaces/System/Web/Script/Serialization/example-2.cs
@@ -1,62 +1,49 @@
 using System;
-using System.Web.Services;
 using System.Web.Script.Serialization;
 
-// [System.Web.Script.Services.ScriptService]
 class C {
 
-//  public class Obj {
-//    public string field1 { get; set; }
-//    public string field2 { get; set; } 
-//  }  
-
-   public class Obj {
-     public Int32  num   { get; set; }
-     public String txt   { get; set; }
-     public String[] ary { get; set; }
-//   public string field2 { get; set; } 
+   #pragma warning disable CS0649 //  Field '…' is never assigned to, and will always have its default value …
+   internal class JSONData {
+      public Int32  num  ;
+      public String txt  ;
+      public String[] ary;
    }  
+   #pragma warning restore CS0649
 
    static void Main() {
 
-       String json = @"
-        { 
-          ""num"":  42,
-          ""txt"":""Hello, World"",
-          ""ary"":[""zero"", ""one"", ""two""]
-        }";
-
+       String JSONText = @"
+       { 
+            ""num"" :   42,
+            ""txt"" : ""Hello, World"",
+            ""ary"" :[
+                       ""zero"",
+                       ""one"" ,
+                       ""two""
+            ]
+       }";
 
        JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
 
-//     System.Collections.Generic.Dictionary<System.String, System.Object[]> 
-//     result =
-//    (System.Collections.Generic.Dictionary<System.String, System.Object[]>)
-//       jsonSerializer.DeserializeObject(@"
-//
-       dynamic result = jsonSerializer.DeserializeObject( json );
+    //
+    // Using DeserializeObject
+    //
+       dynamic result = jsonSerializer.DeserializeObject( JSONText );
+    // Console.WriteLine(result.GetType().FullName);
 
-//     System.Collections.Generic.Dictionary<string, System.Object> result = jsonSerializer.DeserializeObject(@"{ ""field1"":""value1"",""field2"":""value2"" }");
-
-       Console.WriteLine(result.GetType().FullName);
-//     Console.WriteLine(result["num"].GetType().FullName);
        Console.WriteLine(result["num"]);
        Console.WriteLine(result["txt"]);
        Console.WriteLine(result["ary"][2]);
 
-       Obj obj = (Obj) jsonSerializer.Deserialize( json, typeof(Obj) );
+       Console.WriteLine("");
+    //
+    // Using Serialize
+    //
+       JSONData obj = (JSONData) jsonSerializer.Deserialize( JSONText, typeof(JSONData) );
 
-//     Console.WriteLine(result["num"].GetType().FullName);
        Console.WriteLine(obj.num);
        Console.WriteLine(obj.txt);
        Console.WriteLine(obj.ary[2]);
-
-
-
-//    Obj myDeserializedObj = (Obj) JavaScriptConverter.Deserialize(@"{ ""field1"":""value1"",""field2"":""value2"" }", typeof(Obj));
-
-//    Console.WriteLine("field1: {0}", myDeserializedObj.field1);
-
    }
-
 }
