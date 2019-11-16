c989dea07ef305b2fe4fd90082f141648034b83d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-16 08:10:58 +0100

    n/a

--- compilers/c-sharp/strings.cs
+++ compilers/c-sharp/strings.cs
@@ -4,7 +4,13 @@ class C {
 
    static void Main() {
 
+      string txt = "Hello World";
+
       Console.WriteLine( " He said: \"Hello World\". ");      
+
+   //
+   // Verbatim Strings (@"…")
+   //
 
       Console.WriteLine(@" He said ""Hello World"". ");      
 
@@ -12,6 +18,11 @@ class C {
       Console.WriteLine(@" new
  line.");
 //    Console.WriteLine(@" He said \"Hello World\". ");      
+
+   //
+   // Interpolated strings ($"…")
+   //
+      Console.WriteLine($" interpolated> He said: {txt}");
 
    }
 
