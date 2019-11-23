b2fe91a692c91438120b06fe6f7ed0708bd8321c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-23 08:14:46 +0100

    n/a

--- namespaces/Microsoft/Win32/Registry/ex.cs
+++ namespaces/Microsoft/Win32/Registry/ex.cs
@@ -3,18 +3,27 @@ using Microsoft.Win32;
 
 class Prg {
 
+   private static void printPaths(RegistryKey key) {
+
+      foreach (string path in (key.GetValue("PATH") as string).Split(new char[] {';'})) {
+         Console.WriteLine($"  {path}");
+      }
+   }
+
    public static void Main() {
 
       try {
+         Console.WriteLine("Global Env");
          RegistryKey globalEnv = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment");
-         Console.WriteLine("Global path: {0}", globalEnv.GetValue("PATH"));
+         printPaths(globalEnv);
       }
       catch (UnauthorizedAccessException) {
-         Console.WriteLine("Not enough permission to access Environment Key of HKLM");
+         Console.WriteLine("  Not enough permission to access Environment Key of HKLM");
       }
 
       RegistryKey userEnv   = Registry.CurrentUser.CreateSubKey(@"Environment");
-      Console.WriteLine("User path:   {0}", userEnv  .GetValue("PATH"));
+      Console.WriteLine("User Env");
+      printPaths(userEnv);
 
    }
 }
