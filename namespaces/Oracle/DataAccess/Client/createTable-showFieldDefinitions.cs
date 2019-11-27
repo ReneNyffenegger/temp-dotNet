c6329b24525cc667073cb4ddebe93daf4261db9f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-27 10:21:15 +0100

    + ...

---
+++ namespaces/Oracle/DataAccess/Client/createTable-showFieldDefinitions.cs
@@ -0,0 +1,30 @@
+// set ORACLE_HOME=c:\oracle\18c
+// csc -debug -reference:%oracle_home%\ODP.NET\bin\4\Oracle.DataAccess.dll  error.cs
+
+using System;
+using Oracle.DataAccess.Client;
+
+class Prg {
+
+   static void Main() {
+
+      OracleConnection ora = new OracleConnection($"user Id=rene;password=rene;data source=ORA18");
+      ora.Open();
+
+      OracleCommand stmt = ora.CreateCommand();
+      stmt.CommandText = "begin execute immediate 'drop table DataTypeTest'; exception when others then null; end;";
+      stmt.ExecuteNonQuery();
+
+      stmt.CommandText = "create table DataTypeTest (num number(10,3), flt float, rel real)";
+      stmt.ExecuteNonQuery();
+
+      stmt.CommandText = "select * from DataTypeTest";
+      OracleDataReader res = stmt.ExecuteReader(); 
+
+
+      for (int fld=0; fld<res.FieldCount; fld++) {
+         Console.WriteLine($"{res.GetName(fld)}: {res.GetDataTypeName(fld)} - {res.GetFieldType(fld)} - {Type.GetTypeCode(res.GetFieldType(fld))}");
+      }
+
+   }
+}
