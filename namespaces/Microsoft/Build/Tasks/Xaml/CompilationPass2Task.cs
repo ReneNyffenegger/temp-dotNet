f2d8bcdb9cc5675871837c2b728df7d90f8ee0be
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-13 20:16:10 +0100

    n/a

---
+++ namespaces/Microsoft/Build/Tasks/Xaml/CompilationPass2Task.cs
@@ -0,0 +1,9 @@
+//
+//  csc -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\XamlBuildTask.dll" CompilationPass2Task.cs
+
+using Microsoft.Build.Tasks.Xaml;
+
+static class Prg {
+   static void Main() {
+   }
+}
