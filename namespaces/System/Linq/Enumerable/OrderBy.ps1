49c64737dbab8ed4c1d9ebe36bfc3a891792a815
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-03-11 22:34:28 +0100

    Create OrderBy.ps1

---
+++ namespaces/System/Linq/Enumerable/OrderBy.ps1
@@ -0,0 +1,16 @@
+#
+#  The following link was very helpful:
+#    https://www.red-gate.com/simple-talk/dotnet/net-framework/high-performance-powershell-linq/
+#
+
+[System.Collections.Generic.Dictionary[String, Int]] $wordCount = new-object 'System.Collections.Generic.Dictionary[String, Int]'
+
+foreach ($item in 'three', 'four', 'four', 'two', 'four', 'three', 'one', 'two', 'four', 'three') {
+   $wordCount[$item] = $wordCount[$item] + 1
+}
+
+[System.Linq.Enumerable]::OrderBy(
+    $wordCount,
+    [Func[ System.Collections.Generic.KeyValuePair[String, Int] , # This is the type of $p in the next anonymous code block
+           Int                                                    # This is tye type of what the anonymous code block returns
+   ]] { param($p) $p.Value })
