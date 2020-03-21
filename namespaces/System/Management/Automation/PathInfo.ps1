55dadd9ffa39ad0c095b1199e78e5b7fa299cb30
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-03-21 21:29:07 +0100

    n/a

---
+++ namespaces/System/Management/Automation/PathInfo.ps1
@@ -0,0 +1,7 @@
+"Current directory is: $PWD"
+#
+# Current directory is: Microsoft.PowerShell.Core\FileSystem::P:\ath\to\directory
+
+"Current directory is: $($PWD.providerPath)"
+#
+# Current directory is: P:\ath\to\directory
