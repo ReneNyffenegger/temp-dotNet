d11d013f58d85ce0bb4cca73b71ba7f529f2d228
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-08-22 22:08:47 +0200

    n/a

--- namespaces/System/Data/SQLite/download-dll.ps1
+++ namespaces/System/Data/SQLite/download-dll.ps1
@@ -1,2 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 invoke-webRequest https://system.data.sqlite.org/downloads/1.0.113.0/sqlite-netFx40-binary-bundle-Win32-2010-1.0.113.0.zip -outFile sqite-netFx40.dll
-
