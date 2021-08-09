9a066a211329f51bb2cf30bf551f9cff811bef6c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-09 20:58:18 +0200

    n/a

--- namespaces/Microsoft/PowerShell/Commands/ByteCollection/test.ps1
+++ namespaces/Microsoft/PowerShell/Commands/ByteCollection/test.ps1
@@ -1,8 +1,8 @@
 [byte[]] $bytes = [System.Text.Encoding]::ASCII.GetBytes('Hello world.')
 $byteCollection = [Microsoft.PowerShell.Commands.ByteCollection]::new($bytes)
 
-#$byteCollection = new-object Microsoft.PowerShell.Commands.ByteCollection  @($bytes)
-# $byteCollection
+# $byteCollection = new-object Microsoft.PowerShell.Commands.ByteCollection  -argumentList $bytes
+$byteCollection
 
 $byteCollection = new-object Microsoft.PowerShell.Commands.ByteCollection 256, $bytes, "dummy.txt"
 $byteCollection
