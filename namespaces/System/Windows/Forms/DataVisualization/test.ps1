81d50ad01d1943b159b0c73ae51c26e0f58fc922
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-13 23:25:16 +0100

    n/a

--- namespaces/System/Windows/Forms/DataVisualization/test.ps1
+++ namespaces/System/Windows/Forms/DataVisualization/test.ps1
@@ -8,33 +8,30 @@ add-type -assemblyName System.Windows.Forms.DataVisualization
 
 $Chart      = new-object System.Windows.Forms.DataVisualization.Charting.Chart
 $ChartArea  = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
+
 $Series     = New-Object System.Windows.Forms.DataVisualization.Charting.Series
-
-$ChartTypes = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]
-
-$Series.ChartType = $ChartTypes::Pie
+$Series.ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
 
 $Chart.Series.Add($Series)
 $Chart.ChartAreas.Add($ChartArea)
-
 
 
 $Processes = Get-Process | Sort-Object WS -Descending | Select-Object -First 10
 
 $Chart.Series['Series1'].Points.DataBindXY($Processes.Name, $Processes.WS)
 
-$Chart.Width = 700
-$Chart.Height = 400
-$Chart.Left = 10
-$Chart.Top = 10
-$Chart.BackColor = [System.Drawing.Color]::White
-$Chart.BorderColor = 'Black'
+$Chart.Width           = 700
+$Chart.Height          = 400
+$Chart.Left            =  10
+$Chart.Top             =  10
+$Chart.BackColor       = [System.Drawing.Color]::White
+$Chart.BorderColor     = 'Black'
 $Chart.BorderDashStyle = 'Solid'
 
-$ChartTitle = New-Object System.Windows.Forms.DataVisualization.Charting.Title
+$ChartTitle      = New-Object System.Windows.Forms.DataVisualization.Charting.Title
 $ChartTitle.Text = 'Top 5 Processes by Working Set Memory'
-$Font = New-Object System.Drawing.Font @('Microsoft Sans Serif','12', [System.Drawing.FontStyle]::Bold)
-$ChartTitle.Font =$Font
+$Font            = New-Object System.Drawing.Font @('Microsoft Sans Serif','12', [System.Drawing.FontStyle]::Bold)
+$ChartTitle.Font = $Font
 $Chart.Titles.Add($ChartTitle)
 
 $Chart.Series['Series1']['PieLabelStyle'] = 'Disabled'
@@ -45,11 +42,12 @@ $Legend.BorderColor = 'Black'
 $Chart.Legends.Add($Legend)
 $chart.Series['Series1'].LegendText = "#VALX (#VALY)"
 
-$Chart.Series['Series1']['PieLineColor'] = 'Black'
+$Chart.Series['Series1']['PieLineColor' ] = 'Black'
 $Chart.Series['Series1']['PieLabelStyle'] = 'Outside'
-$Chart.Series['Series1'].Label = "#VALX (#VALY)"
+$Chart.Series['Series1'].Label            = "#VALX (#VALY)"
 
-#region Windows Form to Display Chart
+
+
 $AnchorAll = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left
 
 $Form        = New-Object Windows.Forms.Form
@@ -59,11 +57,11 @@ $Form.controls.add($Chart)
 $Chart.Anchor = $AnchorAll
  
 # add a save button
-$SaveButton = New-Object Windows.Forms.Button
-$SaveButton.Text = "Save"
-$SaveButton.Top = 420
-$SaveButton.Left = 600
-$SaveButton.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
+$SaveButton         = New-Object Windows.Forms.Button
+$SaveButton.Text    ="Save"
+$SaveButton.Top     = 420
+$SaveButton.Left    = 600
+$SaveButton.Anchor  = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right
 # [enum]::GetNames('System.Windows.Forms.DataVisualization.Charting.ChartImageFormat')
 $SaveButton.add_click({
     $Result = Invoke-SaveDialog
@@ -74,12 +72,11 @@ $SaveButton.add_click({
 
 if ($true) {
 
-   $ChartArea.Area3DStyle.Enable3D=$True
+   $ChartArea.Area3DStyle.Enable3D    = $True
    $ChartArea.Area3DStyle.Inclination = 50
 
 }
  
 $Form.controls.add($SaveButton)
 $Form.Add_Shown({$Form.Activate()})
-[void]$Form.ShowDialog()
-#endregion Windows Form to Display Chart
+[void] $Form.ShowDialog()
