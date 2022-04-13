2b919528bcb5876c6a557b137f8b0fb186b97c91
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-04-13 11:08:20 +0200

    n/a

---
+++ namespaces/System/Data/SqlClient/import-export/writeBlobToFile.cs
@@ -0,0 +1,35 @@
+//
+//    csc                                                                                                         writeBlobToFile.cs
+//    csc -r:C:\Windows\Microsoft.NET\assembly\GAC_MSIL\MySql.Data\v4.0_8.0.18.0__c5687fc88969c44d\MySql.Data.dll writeBlobToFile.cs
+//
+//    Comapre
+//        C:\Users\Rene\github\.NET-API\MySql\Data\MySqlClient\writeBlobToFile.cs
+//
+
+using System;
+using System.Data;
+using System.IO;
+using System.Data.SqlClient;
+
+class Prg {
+
+   static void Main() {
+      SqlConnection conn = new SqlConnection("Database=tq84_db;Server=.;trusted_connection=true");
+      conn.Open();
+
+      SqlCommand      sql = new SqlCommand("select filename, image from migtable_blob", conn);
+      sql.CommandType     = CommandType.Text;
+      SqlDataReader   rdr = sql.ExecuteReader();
+
+      while (rdr.Read()) {
+         String     filename = rdr.GetString    (0);
+
+         int   blobLen  = (int) rdr.GetBytes(1, 0, null, 0, 0);
+         Byte[] bytes   = new Byte[blobLen];
+         rdr.GetBytes(1, 0, bytes, 0, blobLen);
+
+         Console.WriteLine($"{filename}: {blobLen} bytes");
+         File.WriteAllBytes(filename, bytes);
+      }
+   }
+}
