795f5b8d5b8dc70f4e9ec8146d5f04b0e4ae8119
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-08 23:24:03 +0200

    .

--- namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
+++ namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
@@ -1,1 +1,3 @@
 add-type -path 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Setup.Configuration.Interop.dll'
+
+$conf = new-object Microsoft.VisualStudio.Setup.Configuration.SetupConfigurationClass
