ddd0beb7793f88e06da8be30c5cbf871a2d32d5a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-08 23:41:01 +0200

    .

--- namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
+++ namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
@@ -1,3 +1,11 @@
 add-type -path 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Setup.Configuration.Interop.dll'
 
 $conf = new-object Microsoft.VisualStudio.Setup.Configuration.SetupConfigurationClass
+
+# https://www.cazzulino.com/how-to-get-vsinstallroot.html
+$guid = new-object Guid "177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D"
+$obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
+[Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2] $obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
+[Microsoft.VisualStudio.Setup.Configuration.SetupConfiguration] $obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
+$i = $obj -as [Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2]
+$instance = $i.GetInstanceForCurrentProcess()
