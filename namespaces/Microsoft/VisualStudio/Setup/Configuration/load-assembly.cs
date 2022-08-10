9d56f608236d2c40b9e4c4669f43008802be3443
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-10 22:15:49 +0200

    .

--- namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.cs
+++ namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.cs
@@ -8,6 +8,14 @@ using System;
 class t {
    static public void Main() {
 
+    
+// https://gitlab.informatik.uni-halle.de/ajpqa/client-seitige-web-anwendungen-uebungen/-/blob/98a838e87b8b21357e5ed056df4f9739784db759/projekt/node_modules/node-gyp/lib/Find-VS2017.cs
+   
+
+
+//
+
+
       Type typ = Type.GetTypeFromCLSID(new Guid("177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D"));
       Console.WriteLine("typ");
 
