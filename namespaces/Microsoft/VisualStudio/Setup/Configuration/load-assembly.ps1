bbea479a363a9befc507ea3597866d1b267425da
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-10 21:24:58 +0200

    .

--- namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
+++ namespaces/Microsoft/VisualStudio/Setup/Configuration/load-assembly.ps1
@@ -4,8 +4,12 @@ $conf = new-object Microsoft.VisualStudio.Setup.Configuration.SetupConfiguration
 
 # https://www.cazzulino.com/how-to-get-vsinstallroot.html
 $guid = new-object Guid "177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D"
-$obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
-[Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2] $obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
-[Microsoft.VisualStudio.Setup.Configuration.SetupConfiguration] $obj  = [Activator]::CreateInstance([Type]::GetTypeFromCLSID($guid)) 
+
+# [type] $typ =[Type]::GetTypeFromCLSID($guid)
+  [type] $typ =[System.Runtime.InteropServices.Marshal]::GetTypeFromCLSID($guid)
+
+$obj  = [Activator]::CreateInstance($typ) 
+[Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2] $obj  = [Activator]::CreateInstance($typ) 
+[Microsoft.VisualStudio.Setup.Configuration.SetupConfiguration]   $obj  = [Activator]::CreateInstance($typ)
 $i = $obj -as [Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2]
 $instance = $i.GetInstanceForCurrentProcess()
