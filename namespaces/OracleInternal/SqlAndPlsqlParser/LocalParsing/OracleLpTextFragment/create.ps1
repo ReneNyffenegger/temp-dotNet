5591ce388cdd4b5a773a5dedd0b435897af1bcbf
Nyffenegger René <mail@renenyffenegger.ch>
2025-04-08 08:50:45 +0200

    .

--- namespaces/OracleInternal/SqlAndPlsqlParser/LocalParsing/OracleLpTextFragment/create.ps1
+++ namespaces/OracleInternal/SqlAndPlsqlParser/LocalParsing/OracleLpTextFragment/create.ps1
@@ -1,5 +1,16 @@
-add-type -PSPath c:\Oracle\19\ODP.NET\managed\common\Oracle.ManagedDataAccess.dll
+# Old version (Omis?)
+# add-type -PSPath c:\Oracle\19\ODP.NET\managed\common\Oracle.ManagedDataAccess.dll
+#
+# Version 1 IGS:
+# try { add-type -literalPath C:\Oracle\Client\odp.net\managed\common\Oracle.ManagedDataAccess.dll } catch { out-errorRecord $_ }
+
+# Version 2 IGS:
+using assembly "C:\Oracle\Client\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+using namespace Oracle.ManagedDataAccess.Client
 
 $x = 'select * from dual'
 
+#
+# The property 'new' cannot be found on this object. Verify that the property exists.
+#
 $lpTextFragment = [OracleInternal.SqlAndPlsqlParser.LocalParsing.OracleLpTextFragment]::new($x, 0, $x.Length-1)
