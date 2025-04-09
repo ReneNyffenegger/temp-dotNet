5f4a70fa119392139165b2b7540d1edd92397ae7
Nyffenegger René <mail@renenyffenegger.ch>
2025-04-09 07:59:47 +0200

    .

--- namespaces/OracleInternal/list-members.ps1
+++ namespaces/OracleInternal/list-members.ps1
@@ -1,8 +1,8 @@
 $assembly = [Reflection.Assembly]::LoadFrom("C:\Oracle\Client\odp.net\managed\common\Oracle.ManagedDataAccess.dll");
 
-$assembly.GetTypes()
+# $assembly.GetTypes()
 
 $assembly.GetTypes() | ForEach-Object {
     Write-Output "Type: $($_.FullName)"
-    $_ | Get-Member -MemberType Method, Property
+#   $_ | Get-Member -MemberType Method, Property
 }
