369da150102e84b3d49dfea08dcb89e63baeae78
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2025-11-18 13:55:45 +0100

    .

--- namespaces/System/Net/Sockets/TcpClient/HTTPS.ps1
+++ namespaces/System/Net/Sockets/TcpClient/HTTPS.ps1
@@ -1,23 +1,41 @@
-$conn                                           = new-object System.Net.Sockets.TcpClient('renenyffenegger.ch', 443)
+$domain = 'renenyffenegger.ch'
+$conn  = new-object System.Net.Sockets.TcpClient($domain, 443)
+
 [System.Net.Sockets.NetworkStream] $connStream  = $conn.GetStream()
 
-$reader = new-object System.IO.StreamReader($connStream)
-$writer = new-object System.IO.StreamWriter($connStream)
+
+$sslStream = New-Object System.Net.Security.SslStream($connStream,$false)
+$sslStream.AuthenticateAsClient($domain)
+
+# $reader = new-object System.IO.StreamReader($connStream)
+# $writer = new-object System.IO.StreamWriter($connStream)
+$reader = new-object System.IO.StreamReader($sslStream)
+$writer = new-object System.IO.StreamWriter($sslStream)
 
 $writer.AutoFlush = $true
 
-while ($conn.Connected) {
+$writer.WriteLine('GET / HTTP/1.1')
+$writer.WriteLine("Host: $domain")
+$writer.WriteLine('')
+
+while ($reader.Peek() -ne -1) {
+   write-host ($reader.ReadLine())
+}
+
+return
+
+# while ($conn.Connected) {
 
 # write-host "Peek: $($reader.Peek())"
 # return
 
-  while ($connStream.DataAvailable
+# while ($connStream.DataAvailable
+  while ($reader.DataAvailable
   #  -or $reader.Peek() -ne -1
   ) {
 #     write-host "server> $($reader.ReadLine())"
       write-host "server> $($reader.ReadToEnd())"
   }
 
-  return
 
-}
+# }
